53b3370b7ebc3fed75048af2c86129d6d9e2ef28
Gulyuza <guluza007@gmail.com>
2021-04-26 21:25:28 -0400

    updated database

--- src/db/migrations/20210421184424_createMoviesTable.js
+++ src/db/migrations/20210421184424_createMoviesTable.js
@@ -24,26 +24,3 @@ exports.down = function(knex) {
 //   table.string("image_url");
 //   table.timestamps(true, true);
 // });
-exports.up = function(knex) {
-  return knex.schema.createTable("critics", (table) => {
-      table.increments("critic_id").primary();
-      table.string("preferred_name");
-      table.string("surname");
-      table.string("organization_name");
-      table.timestamps(true, true);
-
-  });
-};
-// 1. guluyza code order
-exports.down = function(knex) {
-  return knex.schema.dropTable("critics");
-};
-
-// 2. thinkful code order
-// return knex.schema.createTable("critics", (table) => {
-//   table.increments("critic_id");
-//   table.string("preferred_name");
-//   table.string("surname");
-//   table.string("organization_name");
-//   table.timestamps(true, true);
-// });
